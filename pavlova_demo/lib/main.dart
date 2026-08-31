318e34c77dcbb8a435589b035587ad45d9dbc341
Nika <nikaellalimosnero@gmail.com>
2026-08-31 12:01:32 +0800

    initial commit

---
+++ pavlova_demo/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:pavlova_demo/pavlova.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'pavlova_demo', home: Pavlova());
+  }
+}
